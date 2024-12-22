579b5cad5b9140164ace13ab9ad0da5d957a9976
franbogarin <bogaarmenteros@gmail.com>
2024-12-22 19:35:01 +0100

    añadir archivo objetos

---
+++ objetos.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS default.objetos;
+CREATE TABLE IF NOT EXISTS default.objetos (
+	id Int32,
+	objetos String,
+	cantidad int64,
+	)
+	ENGINE = MergeTree() ORDER BY id
+	
+INSERT INTO default.countries (*)
+	VALUES (1, 'Mochila', 2),
+		   (2, 'Moviles', 2),
+		   (3, 'Camisetas', '9'),
+		   (4, 'Pantalones', '6')
